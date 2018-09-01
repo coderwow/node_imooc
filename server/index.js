a4efce1f52de782bf4280c0283772ba1059b8262
kevin <zhouyangweb@outlook.com>
2018-09-01 22:33:19 +0800

    ejs引擎模板

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 import Koa from 'koa';
+import views from 'koa-views';
+import path from 'path';
 const app = new Koa();
+app.use(views(path.resolve(__dirname, '../views'), {
+    extension: 'ejs'
+}))
 app.use(async (ctx, next) => {
-    ctx.body = '电影首页'
+    if (ctx.path === '/') {
+        await ctx.render('index', {
+            title: '你好，周杨'
+        })
+    }
 });
 app.listen(2300)
